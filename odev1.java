7b88416eded2e3e851d92123a047c01fedcd753b
YavuzSert <yavuzsert@gmail.com>
2023-03-11 15:02:31 +0300

    ikinci deneme

---
+++ odev1.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a[] = { 1, 2, 4, 5, 6, 7, 9, 3, 56 };
+        int i, max;
+
+        max = a[0];
+        max = a[1];
+        for (i = 0; i < 10; i = i + 1)
+            if (a[i] < 0)
+                max = a[i];
+        for (i = 0; i < 10; i = i + 1)
+            if (a[i] < 0)
+                if (a[i] > max)
+                    max = a[i];
+
+        System.out.println(max);
+
+    }
+
+}
